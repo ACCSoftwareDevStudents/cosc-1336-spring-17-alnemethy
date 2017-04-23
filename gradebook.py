72fd831b7087e97a76929397d8634bdaa5601b4f
alnemethy <albert.nemethy@g.austincc.edu>
2017-04-23 15:03:06 -0500

    Add files via upload

--- gradebook.py
+++ gradebook.py
@@ -1,22 +1,27 @@
-from model.transcript import Transcript
-from data.school_db import SchoolDB
-from data.school_initializer import SchoolInitializer
+from course import Course
+from student import Student
+from enrollment import Enrollment
+from professor import Professor
+from transcript import Transcript
+from school_initializer import SchoolInitializer
+from school_db import SchoolDB
 
 class Gradebook:
 
-    def __init__(self, school_db):
 
+    def __init__(self, school_db):
         self.school_db = school_db
         self.enrollments = school_db.enrollments
-        self.students = school_db.school_initializer.students
+
 
     def main(self):
 
         choice = ''
+        c = Course() ## ??
 
         while choice != 'e':
-            print()
-            choice = self.__display_menu()
+
+            choice = self.__main_menu()
 
             if choice == '1':
 
@@ -24,6 +29,8 @@ class Gradebook:
 
                 if enroll_key in self.enrollments:
                     enroll = self.enrollments.get(enroll_key)
+                    c.trananscript(enrollment)   ###
+                    
 
                     grade = input("Enter grade: ")
                     enroll.grade = grade
@@ -36,9 +43,13 @@ class Gradebook:
                 student_id = int(input("Enter student id: "))
 
                 if student_id in self.students:
-                    transcript = Transcript(self.enrollments)
+
                     student = self.students.get(student_id)
+                    transcript = Transcript(self.enrollments)
                     transcript.print_transcript(student)
+
+                else:
+                    print("Student does not exist")
 
             elif choice == '3':
 
@@ -49,19 +60,20 @@ class Gradebook:
 
                 self.school_db.save_data()
 
-            print()
+    def __main_menu(self):
 
-    def __display_menu(self):
-
-        print("Academic Main Menu")
+        print()
+        print("Academic Menu")
         print()
         print("1) Update Grade")
         print("2) Print Student GPA")
         print("3) Print All Enrollments")
-        print("4) Save Data")
+        print("4) Save Enrollments")
         print()
-        return input("Enter 1, 2, 3, or e to exit")
 
-db = SchoolDB(SchoolInitializer())
-gradebook = Gradebook(db)
+        return input("Enter 1,2,3 or 4.  e to exit: ")
+    
+
+school_db = SchoolDB(SchoolInitializer())
+gradebook = Gradebook(school_db)
 gradebook.main()

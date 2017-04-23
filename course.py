4ebd7a99b5daf825fbea0b838de3e87e89f6ed11
alnemethy <albert.nemethy@g.austincc.edu>
2017-04-23 15:00:40 -0500

    Delete course.py

--- course.py
+++
@@ -1,8 +0,0 @@
-class Course:
-
-    def __init__(self, course_id, title, credit_hours, professor):
-
-        self.course_id = course_id
-        self.title = title
-        self.credit_hours = credit_hours
-        self.professor = professor
